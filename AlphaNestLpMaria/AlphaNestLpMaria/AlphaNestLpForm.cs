04d8a89f71350dc7fbe952b6beaa7262c1c7e017
Maria T <36854099+MariaT7@users.noreply.github.com>
2018-11-05 11:06:58 -0500

    Alphabet
    Nested if statements and looping

---
+++ AlphaNestLpMaria/AlphaNestLpMaria/AlphaNestLpForm.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Created by: Maria
+ * Created on: Day-Month-Year
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #28 - Numbers Nested Loops
+ * This program...
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlphaNestLpMaria
+{
+    public partial class frmAlphaNestLp : Form
+    {
+        public frmAlphaNestLp()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //declare local variables
+            String aLetter;
+            int counter;
+
+            //clear the items from the listbox
+            this.lstAlphabet.Items.Clear();
+
+            for(counter = 65; counter <= 90; counter++)
+            {
+                for (counter = 97; counter <= 122; counter++)
+                //convert aLetter
+                aLetter = Char.ConvertFromUtf32(counter);
+
+                //join character and counter
+                lstAlphabet.Items.Add(aLetter + "->" + counter);
+
+                //List the alphabet in the list box for the user to see
+                this.lstAlphabet.Items.Add(counter);
+
+            }
+        }
+    }
+}
---
+++ NestedlpMaria/NestedlpMaria/NestedLoopForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NestedlpMaria
+{
+    public partial class frmNestedLoop : Form
+    {
+        public frmNestedLoop()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //declare local variables
+            int firstNumber;
+            int secondNumber;
+
+            //clear listbix
+            this.lstNumbers.Items.Clear();
+
+            //loop through the numbers from 0 to 10
+            for (firstNumber = 0; firstNumber <=10; firstNumber++)
+            {
+                //for each of the numbers above, loop thorugh sagain writing beside it the second number from 0 to 10
+                for (secondNumber= 0; secondNumber <=10; secondNumber++)
+                {
+                    this.lstNumbers.Items.Add(firstNumber + " -> " + secondNumber);
+                }
+            }
+        }
+    }
+}
